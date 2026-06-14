bd338a1c9769c5a29aa8bd5096dde35f90d0e89c
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-06-14 07:47:49 +0500

    remove Navbar and Sidebar components from Addresses route

--- src/routes/Addresses.jsx
+++ src/routes/Addresses.jsx
@@ -3,19 +3,13 @@ import FeaturedProj from "../Components/featProjects/FeatProjects"
 import Contact from "../Components/contact/Contact"
 import Cursor from "../Components/cursor/Cursor"
 import Hero from "../Components/hero/Hero"
-import Navbar from "../Components/navbar/Navbar"
 import PracticeProjects from "../Components/projects/Projects"
-import Sidebar from "../Components/sidebar/Sidebar"
 import Skills from "../Skills"
 const Addresses = () => {
   return (
     <div>
       <Cursor />
-      <section id="Homepage">
-        <Navbar />
-        <Hero />
-      </section>
-      <Sidebar />
+      <Hero />
       
       <section id="FeaturedProj"><FeaturedProj /></section>
       <section id="PracticeProjects"><PracticeProjects /></section>
